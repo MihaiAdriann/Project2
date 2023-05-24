e379c47e0dea71bd7add4c8e5a8573f18570b1f5
MIhai Adrian <mihai.adrian995@yahoo.com>
2023-05-24 13:57:11 +0300

    Home Page Add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home'
 
 class App extends React.Component{
   constructor(){
@@ -11,6 +12,7 @@ render(){
   return(
     <div>
       Hello World!
+      <Home/>
     </div>
 
   )
